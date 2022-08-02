657c34542647554eb961262570136ef936355b50
viceodev <profviceo@gmail.com>
2022-08-02 13:10:44 +0100

    isogram_checker and resistor_color

---
+++ isogram_checker/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+bool is_isogram( const char phrase[]){
+    if(phrase == 0){
+        return false;
+    }
+
+    int string_length = strlen(phrase);
+    bool is_isogram_status = true;
+    
+    for(int current_char = 0; current_char < string_length; current_char++){
+        if(is_isogram_status == false){
+            break;
+        }else if(phrase[current_char] == '-' || phrase[current_char] == ' '){
+            continue;
+        }
+        
+        for(int i = 0; i < string_length; i++){
+            if(current_char == i){
+                continue;
+            }
+            ;
+            
+            if(tolower(phrase[current_char]) == tolower(phrase[i])){
+                is_isogram_status = false;
+                break ;
+            }
+        }
+    }
+    
+    return is_isogram_status;
+}
+
+int main() {
+    // Write C code here
+    printf("Hello world %d", is_isogram("victor"));
+
+    return 0;
+}
