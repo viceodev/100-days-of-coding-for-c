12e1e0e1345fdb9e2cd6549b75604715f10b9792
viceodev <profviceo@gmail.com>
2022-08-02 13:11:23 +0100

    update

--- armstrong_number/main.c
+++ armstrong_number/main.c
@@ -2,7 +2,6 @@
 #include <stdbool.h>
 #include <math.h>
 #include <stdlib.h>
-#include <time.h>
 
 
 int intLen(int integer){
@@ -14,11 +13,11 @@ bool is_armstrong_number(int integer){
     int integer_length = intLen(integer);
     int count = integer_length;
     int integer_copy = integer;
-    
+
     while(count > 0){
         int single_int = integer_copy % 10;
         int power = 1;
-        
+
         for(int i = 0; i < integer_length; i++){
             power *= single_int;
         }
@@ -27,32 +26,11 @@ bool is_armstrong_number(int integer){
         integer_copy /= 10;
         count--;
     }
-    
+
     return total == integer;
 }
 
 int main(){
-    double total_time_spent = 0.0;
-    clock_t start = clock();
-
-    int user_integer;
-
-    // printf("Hello! Welcome to Viceo's Armstrong Number Checker.\n");
-    // printf("What number would you like to check: ");
-    // scanf("%d", &user_integer);
-
-    // if(is_armstrong_number(user_integer)){
-    //     printf("%d is an Armstrong Number", user_integer);
-    // }else{
-    //     printf("%d is an not Armstrong Number", user_integer);
-        
-    // }
-
-    printf("%d", is_armstrong_number(9926315));
-
-    clock_t end = clock();
-    total_time_spent += (double)(end - start ) / CLOCKS_PER_SEC;
-    printf("\n\n\n It took your program %f to run", total_time_spent);
-
+    printf("%d", is_armstrong_number(999999999));
     return 0;
 }
---
+++ resistor_color/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+typedef enum {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+} resistor_band_t;
+
+int color_code(int color){
+    resistor_band_t color_code = color;
+    return color_code;
+}
+
+resistor_band_t * colors(){
+    static resistor_band_t color_bands[10];
+
+    for(resistor_band_t band = BLACK; band <= WHITE; band++){
+        color_bands[band] = band;
+    }
+
+    return color_bands;
+}
+
+int main(){
+    printf("Get single color value %d", color_code(WHITE));
+    return 0;
+}
