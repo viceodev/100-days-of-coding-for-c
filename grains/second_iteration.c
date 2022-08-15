9dc783fcfc40bf9b3366293c7f9b86b818f36685
viceodev <profviceo@gmail.com>
2022-08-15 14:00:15 +0100

    grains second iteration

---
+++ grains/second_iteration.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+ * Author: @viceodev [on all social media]
+ * Instruction: https://exercism.org/tracks/c/exercises/grains
+ */
+
+uint64_t square(uint8_t  index){
+    return (index > 0 && index <= 64) ? (1ull << (index - 1)) : 0;
+}
+
+uint64_t total(void){
+    return UINT64_MAX;
+}
+
+int main(){
+    printf("%llu\n", square(0));
+    printf("%llu", total());
+
+    return 0;
+}
